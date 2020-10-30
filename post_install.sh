5e92cb9815ef1545ed5475c9ce13d60ab42c519e
Carlos Gálvez <carlos.galvez@tiendeo.com>
2020-10-30 15:53:14 +0100

    Add visual studio code

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,9 @@
 # Rpm fusion
 sudo dnf -y install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 sudo dnf -y clean all && sudo dnf -y update && sudo dnf -y upgrade
 
 sudo dnf install \
@@ -14,6 +17,7 @@ zsh-syntax-highlighting \
 #powerline-fonts \
 neovim \
 python3-neovim \
+code \
 #libXcomposite \
 #libXcursor \
 #libXi \
